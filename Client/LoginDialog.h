cc11e21141cad73c8db8ada821d4a2fb008d2c9a
simonelopriore <57527781+simonelopriore@users.noreply.github.com>
2020-03-22 12:05:31 +0100

    DialogLogin added

---
+++ Client/LoginDialog.h
@@ -0,0 +1,42 @@
+//
+// Created by Windows on 22/03/2020.
+//
+
+#ifndef CLIENT_LOGINDIALOG_H
+#define CLIENT_LOGINDIALOG_H
+
+
+#include <QDialog>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QDialogButtonBox>
+#include <QGridLayout>
+#include <QPushButton>
+#include <QDebug>
+
+class LoginDialog: public QDialog {
+    Q_OBJECT
+
+public:
+    LoginDialog(QWidget *parent = 0);
+    void setUsername(QString& username);
+    void setPassword(QString& password);
+
+private:
+    QLabel* labelUsername;
+    QLabel* labelPassword;
+    QLineEdit* editUsername;
+    QLineEdit* editPassword;
+    QDialogButtonBox* buttons;
+    void setUpGUI();
+
+signals:
+    void acceptLogin(QString& username, QString& password);
+
+public slots:
+    void slotAcceptLogin();
+
+};
+
+
+#endif //CLIENT_LOGINDIALOG_H
--- Client/cmake-build-debug/CMakeFiles/Client.dir/DependInfo.cmake
+++ Client/cmake-build-debug/CMakeFiles/Client.dir/DependInfo.cmake
@@ -4,14 +4,15 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "D:/poliTO/magistrale/Programmazione di Sistema/Malnati/modifiche_progetto/Client/cmake-build-debug/Client_autogen/mocs_compilation.cpp" "D:/poliTO/magistrale/Programmazione di Sistema/Malnati/modifiche_progetto/Client/cmake-build-debug/CMakeFiles/Client.dir/Client_autogen/mocs_compilation.cpp.obj"
-  "D:/poliTO/magistrale/Programmazione di Sistema/Malnati/modifiche_progetto/Client/Packet/DataPacket.cpp" "D:/poliTO/magistrale/Programmazione di Sistema/Malnati/modifiche_progetto/Client/cmake-build-debug/CMakeFiles/Client.dir/Packet/DataPacket.cpp.obj"
-  "D:/poliTO/magistrale/Programmazione di Sistema/Malnati/modifiche_progetto/Client/Packet/LoginInfo.cpp" "D:/poliTO/magistrale/Programmazione di Sistema/Malnati/modifiche_progetto/Client/cmake-build-debug/CMakeFiles/Client.dir/Packet/LoginInfo.cpp.obj"
-  "D:/poliTO/magistrale/Programmazione di Sistema/Malnati/modifiche_progetto/Client/Packet/Message.cpp" "D:/poliTO/magistrale/Programmazione di Sistema/Malnati/modifiche_progetto/Client/cmake-build-debug/CMakeFiles/Client.dir/Packet/Message.cpp.obj"
-  "D:/poliTO/magistrale/Programmazione di Sistema/Malnati/modifiche_progetto/Client/Packet/Payload.cpp" "D:/poliTO/magistrale/Programmazione di Sistema/Malnati/modifiche_progetto/Client/cmake-build-debug/CMakeFiles/Client.dir/Packet/Payload.cpp.obj"
-  "D:/poliTO/magistrale/Programmazione di Sistema/Malnati/modifiche_progetto/Client/Packet/Symbols/Symbol.cpp" "D:/poliTO/magistrale/Programmazione di Sistema/Malnati/modifiche_progetto/Client/cmake-build-debug/CMakeFiles/Client.dir/Packet/Symbols/Symbol.cpp.obj"
-  "D:/poliTO/magistrale/Programmazione di Sistema/Malnati/modifiche_progetto/Client/SharedEditor.cpp" "D:/poliTO/magistrale/Programmazione di Sistema/Malnati/modifiche_progetto/Client/cmake-build-debug/CMakeFiles/Client.dir/SharedEditor.cpp.obj"
-  "D:/poliTO/magistrale/Programmazione di Sistema/Malnati/modifiche_progetto/Client/main.cpp" "D:/poliTO/magistrale/Programmazione di Sistema/Malnati/modifiche_progetto/Client/cmake-build-debug/CMakeFiles/Client.dir/main.cpp.obj"
+  "C:/Users/Windows/Documents/GitHub/SharedEditor_MalnatiProject/Client/cmake-build-debug/Client_autogen/mocs_compilation.cpp" "C:/Users/Windows/Documents/GitHub/SharedEditor_MalnatiProject/Client/cmake-build-debug/CMakeFiles/Client.dir/Client_autogen/mocs_compilation.cpp.obj"
+  "C:/Users/Windows/Documents/GitHub/SharedEditor_MalnatiProject/Client/LoginDialog.cpp" "C:/Users/Windows/Documents/GitHub/SharedEditor_MalnatiProject/Client/cmake-build-debug/CMakeFiles/Client.dir/LoginDialog.cpp.obj"
+  "C:/Users/Windows/Documents/GitHub/SharedEditor_MalnatiProject/Client/Packet/DataPacket.cpp" "C:/Users/Windows/Documents/GitHub/SharedEditor_MalnatiProject/Client/cmake-build-debug/CMakeFiles/Client.dir/Packet/DataPacket.cpp.obj"
+  "C:/Users/Windows/Documents/GitHub/SharedEditor_MalnatiProject/Client/Packet/LoginInfo.cpp" "C:/Users/Windows/Documents/GitHub/SharedEditor_MalnatiProject/Client/cmake-build-debug/CMakeFiles/Client.dir/Packet/LoginInfo.cpp.obj"
+  "C:/Users/Windows/Documents/GitHub/SharedEditor_MalnatiProject/Client/Packet/Message.cpp" "C:/Users/Windows/Documents/GitHub/SharedEditor_MalnatiProject/Client/cmake-build-debug/CMakeFiles/Client.dir/Packet/Message.cpp.obj"
+  "C:/Users/Windows/Documents/GitHub/SharedEditor_MalnatiProject/Client/Packet/Payload.cpp" "C:/Users/Windows/Documents/GitHub/SharedEditor_MalnatiProject/Client/cmake-build-debug/CMakeFiles/Client.dir/Packet/Payload.cpp.obj"
+  "C:/Users/Windows/Documents/GitHub/SharedEditor_MalnatiProject/Client/Packet/Symbols/Symbol.cpp" "C:/Users/Windows/Documents/GitHub/SharedEditor_MalnatiProject/Client/cmake-build-debug/CMakeFiles/Client.dir/Packet/Symbols/Symbol.cpp.obj"
+  "C:/Users/Windows/Documents/GitHub/SharedEditor_MalnatiProject/Client/SharedEditor.cpp" "C:/Users/Windows/Documents/GitHub/SharedEditor_MalnatiProject/Client/cmake-build-debug/CMakeFiles/Client.dir/SharedEditor.cpp.obj"
+  "C:/Users/Windows/Documents/GitHub/SharedEditor_MalnatiProject/Client/main.cpp" "C:/Users/Windows/Documents/GitHub/SharedEditor_MalnatiProject/Client/cmake-build-debug/CMakeFiles/Client.dir/main.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
@@ -27,14 +28,14 @@ set(CMAKE_TARGET_DEFINITIONS_CXX
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
   "Client_autogen/include"
-  "C:/Qt/5.12.5/mingw73_64/include"
-  "C:/Qt/5.12.5/mingw73_64/include/QtCore"
-  "C:/Qt/5.12.5/mingw73_64/./mkspecs/win32-g++"
-  "C:/Qt/5.12.5/mingw73_64/include/QtGui"
-  "C:/Qt/5.12.5/mingw73_64/include/QtANGLE"
-  "C:/Qt/5.12.5/mingw73_64/include/QtWidgets"
-  "C:/Qt/5.12.5/mingw73_64/include/QtCharts"
-  "C:/Qt/5.12.5/mingw73_64/include/QtNetwork"
+  "C:/Qt/5.13.1/mingw73_64/include"
+  "C:/Qt/5.13.1/mingw73_64/include/QtCore"
+  "C:/Qt/5.13.1/mingw73_64/./mkspecs/win32-g++"
+  "C:/Qt/5.13.1/mingw73_64/include/QtGui"
+  "C:/Qt/5.13.1/mingw73_64/include/QtANGLE"
+  "C:/Qt/5.13.1/mingw73_64/include/QtWidgets"
+  "C:/Qt/5.13.1/mingw73_64/include/QtCharts"
+  "C:/Qt/5.13.1/mingw73_64/include/QtNetwork"
   )
 
 # Targets to which this target links.
