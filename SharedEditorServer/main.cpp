e2a1e74a5b32391f4ee67259166b6cb8a51cb197
emanuelemanciuria96 <57526741+emanuelemanciuria96@users.noreply.github.com>
2020-03-26 16:33:16 +0100

    Update main.cpp

--- SharedEditorServer/main.cpp
+++ SharedEditorServer/main.cpp
@@ -7,9 +7,7 @@ int main(int argc, char **argv) {
     NetworkServer s;   //il server accetta la connessione di un solo client e simula il comportamento di un secondo clie
                         // con siteId=1 alla ricezione di 'c' e 'a' da parte del client (infatti implementa quasi tutte le funzioni di un client)
 
-    int i = 0;
-    for(;i<5;i++)
-        std::cout<<"diocane fai il commit!!!";
+    
     s.startServer();
     app.exec();
     return 0;
