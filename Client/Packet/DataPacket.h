12f47b03061ed3da30a9a10b4004b59b3372e503
emanuelemanciuria96 <57526741+emanuelemanciuria96@users.noreply.github.com>
2020-04-01 13:40:11 +0200

    Add files via upload

--- Client/Packet/DataPacket.h
+++ Client/Packet/DataPacket.h
@@ -10,28 +10,28 @@
 #include "Payload.h"
 
 class DataPacket {
-private:
-    qint32 source;   //questo campo sarà riempito con il siteID nel caso in cui si conosca, -1 altrimenti
-    quint32 errcode;
-    quint32 type_of_data;
-    std::shared_ptr<Payload> payload;
-
 public:
-    DataPacket(qint32 source, quint32 errcode, quint32 typeOfData);
+    typedef enum Constants
+    {
+        textTyping = 0,
+        login = 1,
+        command = 2,
+    }data_t;
+
+    DataPacket(qint32 source, quint32 errcode, data_t typeOfData,Payload* pl= nullptr);
     qint32 getSource() const;
     void setSource(qint32 source);
     quint32 getErrcode() const;
     void setErrcode(quint32 errcode);
     quint32 getTypeOfData() const;
-    void setTypeOfData(quint32 typeOfData);
-    std::shared_ptr<Payload> &getPayload();
-
-    enum Constants
-    {
-        textTyping = 0,
-        login = 1,
-        idResponse = 2,
-    };
+    void setTypeOfData(data_t typeOfData);
+    std::shared_ptr<Payload> getPayload();
+    void setPayload(std::shared_ptr<Payload> pl);
+private:
+    qint32 source;   //questo campo sarà riempito con il siteID nel caso in cui si conosca, -1 altrimenti
+    quint32 errcode;
+    data_t type_of_data;
+    std::shared_ptr<Payload> payload;
 };
 
 
