245ca3f7b163bf48a1f894c68eef302d666d1f99
emanuelemanciuria96 <emanuele.manc96@gmail.com>
2020-07-11 17:01:45 +0200

    MaxChar da 10000 a 500

--- SharedEditorServer/Packet/StringMessages.h
+++ SharedEditorServer/Packet/StringMessages.h
@@ -46,7 +46,7 @@ private:
     const int items_separatorSize=items_separator.size();
     const int pos_separatorSize=pos_separator.size();
 
-    const int maxChar=10000;
+    const int maxChar=500;
     QString formattedMessages;
 
 };
