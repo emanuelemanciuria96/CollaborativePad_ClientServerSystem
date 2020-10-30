224e42fc519532b8a898dcef42f2342f4cca8791
TM111 <42980799+TM111@users.noreply.github.com>
2020-10-30 18:57:45 +0100

    fix transparentMouse

--- Client/MainWindow.cpp
+++ Client/MainWindow.cpp
@@ -258,7 +258,9 @@ void MainWindow::transparentForMouse() {
     toolBar->setAttribute(Qt::WA_TransparentForMouseEvents, var);
     richTextBar->setAttribute(Qt::WA_TransparentForMouseEvents, var);
     dockWidgetUsers->setAttribute(Qt::WA_TransparentForMouseEvents, var);
-    editor->textEdit->setDisabled(var);
+    editor->setAttribute(Qt::WA_TransparentForMouseEvents, var);
+    editor->textEdit->setDisabled(true);
+    editor->textEdit->setDisabled(false);
     this->uriWidget->setDisabled(var);
     gridView->setAttribute(Qt::WA_TransparentForMouseEvents, var);
     treeView->setAttribute(Qt::WA_TransparentForMouseEvents, var);
